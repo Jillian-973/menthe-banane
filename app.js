c1d962037820ab1f2400ae47ef94dacc3b199b37
Jillian-973 <killiandumoulin02@gmail.com>
2026-03-16 19:13:32 +0100

    all html done

---
+++ app.js
@@ -0,0 +1,9 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(q => {
+  q.addEventListener("click", () => {
+    const item = q.parentElement;
+
+    item.classList.toggle("active");
+  });
+});
